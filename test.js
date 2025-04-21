3b4607cbcedb3af96ab94778f6a5df3c52482a9c
gh-garcia <gabyhg000@proton.me>
2025-04-21 11:56:51 -0400

    Add: CRT styling, remove code glow, fix flicker, apply retro UI

---
+++ test.js
@@ -0,0 +1,6 @@
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = chrome.runtime.getURL("style.css");
+document.head.appendChild(link);
+console.log("🎉 CRT Style applied");
